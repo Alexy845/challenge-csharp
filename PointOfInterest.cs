5f24237d7f2c5271c1456fc1c819969b7fcb6747
Alexy845 <alexy.daubresse@ynov.com>
2023-10-09 16:16:38 +0200

    PointOfInterest

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -7,6 +7,19 @@ namespace CSharpDiscovery.Quest03
         public double Latitude { get; set; }
         public double Longitude { get; set; }
         public string Name { get; set; }
+
+        public PointOfInterest()
+        {
+            Latitude = 44.854186;
+            Longitude = -0.5663056;
+            Name = "Bordeaux Ynov Campus";
+        }
+        public PointOfInterest(double latitude, double longitude, string name)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            Name = name;
+        }
  
         public static string GoogleMapsUrlTemplate { get { return "https://www.google.com/maps/place/{0}/@{1},{2},15z/"; } }
     }
