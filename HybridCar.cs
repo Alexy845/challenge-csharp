3431072bb9fa0d06a07e1bec99fceffb60516e8d
Alexy845 <alexy.daubresse@ynov.com>
2023-10-10 12:17:55 +0200

    HybridCar

---
+++ HybridCar.cs
@@ -0,0 +1,37 @@
+namespace CSharpDiscovery.Quest04 
+{
+    public class HybridCar : Car, IThermalCar, IElectricCar
+    {
+        public HybridCar() : base()
+        {
+
+        }
+        public HybridCar(string model, string brand, string color, int currentSpeed = 0) : base(model, brand, color, currentSpeed)
+        {
+            currentSpeed = 0;
+
+        }
+        public int FuelLevel { get; set; }
+        public int BatteryLevel { get; set; }
+        public int GetFuelLevel()
+        {
+            return FuelLevel;
+        }
+        public int GetBatteryLevel()
+        {
+            return BatteryLevel;
+        }
+        public void FillUp()
+        {
+            FuelLevel = 100;
+        }
+        public void Recharge()
+        {
+            BatteryLevel = 100;
+        }
+        public override string ToString()
+        {
+            return Color + " " + Brand + " " + Model + ", Battery: " + BatteryLevel + "%, Fuel: " + FuelLevel + "%";
+        }
+    }
+}
