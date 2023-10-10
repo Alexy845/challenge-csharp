092d8a6d66428b0eb0a02cf9423658189eee9436
Alexy845 <alexy.daubresse@ynov.com>
2023-10-10 11:06:22 +0200

    Car

--- Car.cs
+++ Car.cs
@@ -4,6 +4,23 @@ namespace CSharpDiscovery.Quest04
     public class Car : Vehicule
     {
         public string Model { get; set; }
+
+        public override void Accelerate(int Speed)
+        {
+            CurrentSpeed += Speed;
+            if (CurrentSpeed > 180)
+            {
+                CurrentSpeed = 180;
+            }
+        }
+        public override void Brake(int BrakePower)
+        {
+            CurrentSpeed -= BrakePower;
+            if (CurrentSpeed < 0)
+            {
+                CurrentSpeed = 0;
+            }
+        }
         public Car() : base()
         {
             Model = "Unknown";
@@ -12,7 +29,6 @@ namespace CSharpDiscovery.Quest04
         {
             this.Model = model;
         }
-        
         public override string ToString()
         {
             return Color + " " + Brand + " " + Model;
